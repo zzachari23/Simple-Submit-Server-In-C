98f2338e67999b6e69db1b999234ee754c18f3f0
Zachariah Zachariah <zachgit1999@gmail.com>
2023-07-13 18:31:32 -0400

    Add files via upload

---
+++ safe-fork.h
@@ -0,0 +1,6 @@
+#if !defined(SAFE_FORK_H)
+#define SAFE_FORK_H
+
+int safe_fork(void);
+
+#endif
---
+++ split.h
@@ -0,0 +1,7 @@
+#if !defined(SPLIT_H)
+#define SPLIT_H
+
+
+char **split(const char line[]);
+
+#endif
